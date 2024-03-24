206f0c30751e24e7ddde750548449c55c8a2b492
Max <maxcatalina@iMac-Max.local>
2024-03-24 20:56:14 +0200

    w

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-const inputField = document.querySelector('.text-field');
-const hideDiv = document.querySelector('.hide-div');
+const slides = document.querySelectorAll('.slider-item');
+const prev = document.querySelector('.slider-prev');
+const next = document.querySelector('.slider-next');
+let index = 0;
 
-inputField.addEventListener('focus', function () {
-  hideDiv.classList.add('show');
+function showSlide(slideIndex) {
+    slides.forEach(slide => {
+        slide.classList.remove('show');
+    });
+    slides[slideIndex].classList.add('show');
+}
+showSlide(index);
+
+next.addEventListener('click', function () {
+    if (index < slides.length - 1) {
+        index++;
+        showSlide(index);
+    }
+    prev.classList.toggle('disable', index === 0);
+    next.classList.toggle('disable', index === slides.length - 1);
 });
-inputField.addEventListener('blur', function () {
-  hideDiv.classList.remove('show');
+
+prev.addEventListener('click', function () {
+    if (index > 0) {
+        index--;
+        showSlide(index);
+    }
+    prev.classList.toggle('disable', index === 0);
+    next.classList.toggle('disable', index === slides.length - 1);
 });
